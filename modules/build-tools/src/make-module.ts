d5f628120d52282b12c7a44057aecca6899728ce
Kevin Frei <kevinfrei@hotmail.com>
2024-08-01 22:18:36 -0700

    Trying to switch from terser to esbuild

--- modules/build-tools/src/make-module.ts
+++ modules/build-tools/src/make-module.ts
@@ -1,44 +1,94 @@
-// Taken from
-// https://adostes.medium.com/authoring-a-javascript-library-that-works-everywhere-using-rollup-f1b4b527b2a9
+import * as esbuild from 'esbuild';
+import { promises as fs } from 'fs';
+import * as ts from "typescript";
 
-import nodeResolve from '@rollup/plugin-node-resolve';
-import { OutputOptions, rollup, RollupOptions } from 'rollup';
-import dts from 'rollup-plugin-dts';
+type ModuleOptions = {
+  dir: string;
+  format: 'esm' | 'cjs';
+  exports: 'named' | 'default';
+  platform?: 'node' | 'browser';
+  sourcemap: boolean;
+};
 
-const esmOptions: OutputOptions = {
-  dir: 'dist/esm',
+const esmOptions: ModuleOptions = {
+  dir: 'lib/esm',
   format: 'esm',
   exports: 'named',
-  sourcemap: true,
+  platform: 'node',
+  sourcemap: false,
 };
 
-const cjsOptions: OutputOptions = {
-  dir: 'dist/cjs',
+const cjsOptions: ModuleOptions = {
+  dir: 'lib/cjs',
   format: 'cjs',
   exports: 'named',
-  sourcemap: true,
+  platform: 'node',
+  sourcemap: false,
 };
 
-async function genBundle(opts: RollupOptions): Promise<void> {
-  const bundle = await rollup(opts);
-  await bundle.generate(esmOptions);
-  await bundle.write(esmOptions);
-  await bundle.generate(cjsOptions);
-  await bundle.write(cjsOptions);
-  await bundle.close();
+async function genBundle(entryPoints: string[], opts: ModuleOptions): Promise<void> {
+  await esbuild.build({
+    entryPoints,
+    bundle: true,
+    outdir: opts.dir,
+    format: opts.format,
+    target: 'esnext',
+    platform: 'node',
+    minify: true,
+    external: ['esbuild']
+  });
+  // Write the package.json file for the module
+  if (opts.format === 'esm') {
+    await fs.writeFile('lib/esm/package.json', JSON.stringify({ type: 'module' }));
+  } else {
+    await fs.writeFile('lib/cjs/package.json', JSON.stringify({ type: 'commonjs' }));
+  }
 }
 
+function compile(fileNames: string[], options: ts.CompilerOptions): void {
+  // Create a Program with an in-memory emit
+  const createdFiles: { [key: string]: string } = {}
+  const host = ts.createCompilerHost(options);
+  host.writeFile = (fileName: string, contents: string) => createdFiles[fileName] = contents
+
+  // Prepare and emit the d.ts files
+  const program = ts.createProgram(fileNames, options, host);
+  program.emit();
+
+  // Loop through all the input files
+  fileNames.forEach(file => {
+    console.log("### JavaScript\n")
+    console.log(host.readFile(file))
+
+    console.log("### Type Definition\n")
+    const dts = file.replace(".js", ".d.ts")
+    console.log(createdFiles[dts]);
+  });
+}
+
+function genTypes(): void {
+  // Run the typescript compiler to generate the .d.ts files
+  compile(process.argv.slice(2), {
+    allowJs: true,
+    declaration: true,
+    emitDeclarationOnly: true,
+  });
+}
+
+// First, transpile the code for ESM and CJS, then use the typescript compiler
+// to generate the .d.ts files as well.
+
 export async function makeDualModeModule(root: string[]): Promise<number> {
-  const nodeRes = nodeResolve();
-  // Generate the code
-  await genBundle({
-    input: root,
-    plugins: [nodeRes],
-  });
+  // Generate the CJS code
+  console.log(root);
+  console.log("CJS")
+  await genBundle(root, { ...cjsOptions, sourcemap: root[0] == '--debug' });
+  // Generate the ESM code
+  console.log("ESM")
+  await genBundle(root, esmOptions);
   // Now generate the types
-  await genBundle({
-    input: root,
-    plugins: [nodeRes, dts()],
-  });
+  console.log("TypeScript")
+  await genTypes();
+  console.log("Done")
   return 0;
 }
