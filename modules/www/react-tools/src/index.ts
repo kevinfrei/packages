a2733a4d5442594d821fe6159500bd0da02a98b1
Kevin Frei <kevinfrei@hotmail.com>
2026-05-02 23:36:30 -0700

    Undid the version stuff

--- modules/www/react-tools/src/index.ts
+++ modules/www/react-tools/src/index.ts
@@ -1,4 +1,5 @@
 export * from './Helpers.js';
 export * from './Hooks.js';
 export * from './ReactHelp.js';
-export * as Layout from './Layout.js';
+import * as Layout from './Layout.js';
+export { Layout };
